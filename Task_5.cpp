a043a5c5391a2f16f47b096b94d9398f676b17f9
ksywxd <xenia5224@gmail.com>
2025-09-19 00:00:56 +0300

    control

--- Task_5.cpp
+++ Task_5.cpp
@@ -2,7 +2,18 @@
 
 int main()
 {
+    int X, Y, max;
     std::cin >> X >> Y;
-
+    if (X > Y) {
+        max = X; 
+        std::cout << "MAX VERSTAPPEN: " << max;
+    }
+    else {
+        if (Y > X) {
+            max = Y;
+            std::cout << "MAX VERSTAPPEN: " << max;
+        }
+        else std::cout << "RAVNI";
+    }
     return 0;
 }
